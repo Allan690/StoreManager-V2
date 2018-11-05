685bb1025615e249fd94a097ac21a9ec3334cd05
Allan069 <allannelly690@gmail.com>
2018-11-05 21:19:00 +0300

    Added index route to heroku page

--- app/__init__.py
+++ app/__init__.py
@@ -27,3 +27,14 @@ def handle_405_errors(e):
     ), 405
 
 
+@flask_app.errorhandler(500)
+def handle_500_errors(e):
+    return jsonify(
+        {"Message": "There was an internal server error."}
+    ), 500
+
+
+@flask_app.route('/')
+def index_route():
+    """Defines the index route"""
+    return "<p>Find the app documentation <a href='https://apimatic.io/apidocs/storemanager-api-v2/'>here</a></p>"
